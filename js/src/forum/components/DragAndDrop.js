b32be4617c7d3b451668cf6bc2461179e643374d
skywalker <houzhenhong@hotmail.com>
2020-03-15 23:31:09 +0800

    fix(upload): 拖动上传文字出错

--- js/src/forum/components/DragAndDrop.js
+++ js/src/forum/components/DragAndDrop.js
@@ -6,17 +6,23 @@ export default class DragAndDrop {
         // Keep references to the bound methods so we can remove the event listeners later
         this.handlers = {};
 
+        this.composerElement.addEventListener('dragstart', this.handlers.start = this.start.bind(this));
         this.composerElement.addEventListener('dragover', this.handlers.in = this.in.bind(this));
-
         this.composerElement.addEventListener('dragleave', this.handlers.out = this.out.bind(this));
         this.composerElement.addEventListener('dragend', this.handlers.out);
 
         this.composerElement.addEventListener('drop', this.handlers.dropping = this.dropping.bind(this));
 
         this.isDropping = this.over = false;
+        this.isCap = true;
+    }
+
+    start() {
+        this.isCap = false;
     }
 
     unload() {
+        this.composerElement.removeEventListener('dragstart', this.handlers.start);
         this.composerElement.removeEventListener('dragover', this.handlers.in);
 
         this.composerElement.removeEventListener('dragleave', this.handlers.out);
@@ -26,6 +32,8 @@ export default class DragAndDrop {
     }
 
     in(event) {
+        if (!this.isCap) return;
+        console.log('over')
         event.preventDefault();
 
         if (!this.over) {
@@ -35,6 +43,7 @@ export default class DragAndDrop {
     }
 
     out(event) {
+        if (!this.isCap) return;
         event.preventDefault();
 
         if (this.over) {
@@ -44,6 +53,11 @@ export default class DragAndDrop {
     }
 
     dropping(event) {
+        console.log('dropping');
+        if (!this.isCap) {
+            this.isCap = true;
+            return;
+        }
         event.preventDefault();
 
         if (!this.isDropping) {
